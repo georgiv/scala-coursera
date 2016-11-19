1fec12d32b26ebcc8a31c622957431943657d4df
georgiv <gd.vassilev@gmail.com>
2016-11-19 10:20:39 +0200

    Setting home repository

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -15,39 +15,38 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = {
-    if ((r == 0) || (c == 0) || (c == r)) 1
-    else pascal(c - 1, r - 1) + pascal(c, r - 1)
-  }
-
+    def pascal(c: Int, r: Int): Int = {
+      if ((r == 0) || (c == 0) || (c == r)) 1
+      else pascal(c - 1, r - 1) + pascal(c, r - 1)
+    }
+  
   /**
    * Exercise 2
    */
-
-  def balance(chars: List[Char]): Boolean = {
-    @tailrec
-    def iterate(c: Int, remainder: List[Char]): Boolean = {
-      if (c < 0) return false
-      if (remainder.isEmpty) (c == 0)
-      else if (remainder.head.equals('(')) iterate(c + 1, remainder.tail)
-      else if (remainder.head.equals(')')) iterate(c - 1, remainder.tail)
-      else iterate(c, remainder.tail)
+    def balance(chars: List[Char]): Boolean = {
+      @tailrec
+      def iterate(c: Int, remainder: List[Char]): Boolean = {
+        if (c < 0) return false
+        if (remainder.isEmpty) (c == 0)
+        else if (remainder.head == '(') iterate(c + 1, remainder.tail)
+        else if (remainder.head == ')') iterate(c - 1, remainder.tail)
+        else iterate(c, remainder.tail)
+      }
+      iterate(0, chars)
     }
-    iterate(0, chars)
-  }
-
+  
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
-    def iterate(result: Int, sum: Int, remainder: List[Int]): Int = {
-      if (sum == money) 
-        result + 1
-      else if (sum < money && !remainder.isEmpty)
-        iterate(result, sum + remainder.head, remainder) + iterate(result, sum, remainder.tail)
-      else 
-        result
+    def countChange(money: Int, coins: List[Int]): Int = {
+      def iterate(result: Int, sum: Int, remainder: List[Int]): Int = {
+        if (sum == money) 
+          result + 1
+        else if (sum < money && !remainder.isEmpty) 
+          iterate(result, sum + remainder.head, remainder) + iterate(result, sum, remainder.tail)
+        else
+          result
+      }
+      iterate(0, 0, coins)
     }
-    iterate(0, 0, coins)
   }
-}
