9633cd94cf9566f4681d44bd6c71eb7396a6abc9
georgiv <gd.vassilev@gmail.com>
2016-12-14 20:55:45 +0200

    Working on quickcheck

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -26,32 +26,21 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(insert(Int.MinValue, h)) == Int.MinValue
   }
 
-  property("delete") = forAll { (n: Int) =>
-    val h = deleteMin(insert(n, empty))
-    isEmpty(h) == true
+  property("deleteMin") = forAll { (n: Int) =>
+    isEmpty(deleteMin(insert(n, empty)))
   }
 
-  property("insert two elements into empty heap and find the minimum") = forAll { (n: Int, m: Int) =>
-    val h = insert(m, insert(n, empty))
-    findMin(h) == Math.min(n, m)
+  property("insert2") = forAll { (n: Int, m: Int) =>
+    findMin(insert(m, insert(n, empty))) == Math.min(n, m)
   }
-//
-//  property("insert an element into empty heap and delete the minimum") = forAll { (n: Int) =>
-//    val h = deleteMin(insert(n, empty))
-//    isEmpty(h) == true
-//  }
-//
+
+  property("meld") = forAll { (h1: H, h2: H) =>
+    findMin(meld(h1, h2)) == Math.min(findMin(h1), findMin(h2))
+  }
+
 //  property("sorted sequence from heap") = forAll { (h: H) => 
 //    def sort(h: H, sorted: List[A]):List[A] = if (isEmpty(h)) sorted.reverse else sort(deleteMin(h), findMin(h)::sorted)
 //    val l = sort(h, Nil)
 //    l == l.sorted
-//  }
-//
-//  property("find min after meld") = forAll { (h1: H, h2: H) =>
-//    val h3 = meld(h1, h2)
-//    if (isEmpty(h3)) throw new NoSuchElementException("min of empty heap")
-//    else if (isEmpty(h1) && !isEmpty(h2)) findMin(h3) == findMin(h2)
-//    else if (!isEmpty(h1) && isEmpty(h2)) findMin(h3) == findMin(h1)
-//    else findMin(h3) == Math.min(findMin(h1), findMin(h2))
 //  }
 }
