8725560f3a5f44e488ce8fd2718f07aa6fca56ab
georgiv <gd.vassilev@gmail.com>
2016-12-14 22:01:34 +0200

    Course2-Week3-quickcheck completed

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -15,32 +15,13 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
-  // 1 & 2
   property("findMin") = forAll { (h: H) =>
     val m = if (isEmpty(h)) 0 else findMin(h)
     findMin(insert(m, h)) == m
   }
 
-  // 2
-  property("insert") = forAll { (h: H) =>
-    findMin(insert(Int.MinValue, h)) == Int.MinValue
+  property("insert + deleteMin") = {
+    val h1 = insert(11, insert(3, insert(8, empty)))
+    deleteMin(h1) == insert(11, insert(8, empty))
   }
-
-  property("deleteMin") = forAll { (n: Int) =>
-    isEmpty(deleteMin(insert(n, empty)))
-  }
-
-  property("insert2") = forAll { (n: Int, m: Int) =>
-    findMin(insert(m, insert(n, empty))) == Math.min(n, m)
-  }
-
-  property("meld") = forAll { (h1: H, h2: H) =>
-    findMin(meld(h1, h2)) == Math.min(findMin(h1), findMin(h2))
-  }
-
-//  property("sorted sequence from heap") = forAll { (h: H) => 
-//    def sort(h: H, sorted: List[A]):List[A] = if (isEmpty(h)) sorted.reverse else sort(deleteMin(h), findMin(h)::sorted)
-//    val l = sort(h, Nil)
-//    l == l.sorted
-//  }
 }
